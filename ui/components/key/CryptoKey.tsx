d49ca70916424e58ee41cbf26d55ef1023fbd322
Paul Kinlan <paul.kinlan@gmail.com>
2023-02-26 19:37:28 +0000

    formatting

--- ui/components/key/CryptoKey.tsx
+++ ui/components/key/CryptoKey.tsx
@@ -5,16 +5,14 @@ import { encode } from "https://deno.land/std@0.177.0/encoding/hex.ts";
 
 import { Heading4 } from "../general/Headings.tsx";
 
-import {
-  exportTypes
-} from "../../defaults.ts";
+import { exportTypes } from "../../defaults.ts";
 
 function encodeToString(buffer: Uint8Array): string {
   return new TextDecoder().decode(buffer);
 }
 
 function exportKey(binaryData: Uint8Array, label: string): string {
-  if (label == 'secret') {
+  if (label == "secret") {
     return encodeToString(encode(binaryData));
   }
 
@@ -27,9 +25,9 @@ function ab2str(buf) {
 
 function convertBinaryToPem(binaryData: Uint8Array, label: string): string {
   let base64Cert = btoa(ab2str(binaryData));
-  
+
   let newLabel = (label == "public") ? "PUBLIC KEY" : "PRIVATE KEY";
-  let pemCert = "-----BEGIN " + newLabel + "-----\r\n"
+  let pemCert = "-----BEGIN " + newLabel + "-----\r\n";
   let nextIndex = 0;
   let lineLength;
   while (nextIndex < base64Cert.length) {
@@ -44,18 +42,22 @@ function convertBinaryToPem(binaryData: Uint8Array, label: string): string {
   return pemCert;
 }
 
-export function CryptoKey({keyState}): JSX.Element {
+export function CryptoKey({ keyState }): JSX.Element {
   const [exportedKeyState, setExportedKeyState] = useState("");
   let exportKeyType = "";
   useEffect(() => {
     //https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/exportKey
-    if (keyState.type == 'public' && 'public' in exportTypes[keyState.algorithm.name]) {
+    if (
+      keyState.type == "public" &&
+      "public" in exportTypes[keyState.algorithm.name]
+    ) {
       exportKeyType = exportTypes[keyState.algorithm.name].public;
-    }
-    else if (keyState.type == 'private' && 'private' in exportTypes[keyState.algorithm.name]) {
+    } else if (
+      keyState.type == "private" &&
+      "private" in exportTypes[keyState.algorithm.name]
+    ) {
       exportKeyType = exportTypes[keyState.algorithm.name].private;
-    }
-    else {
+    } else {
       // keyState.type == 'secret'
       exportKeyType = exportTypes[keyState.algorithm.name][0];
     }
@@ -63,7 +65,7 @@ export function CryptoKey({keyState}): JSX.Element {
     crypto.subtle.exportKey(exportKeyType, keyState)
       .then((exportedKey) => {
         setExportedKeyState(exportedKey);
-        console.log(exportedKey)
+        console.log(exportedKey);
         return exportedKey;
       }).catch((err) => {
         console.error(err);
